05bc5318936920a16bfaaaeb0e7f74a1f02b47fa
Abraham Esparza <abrahamesparza.us@gmail.com>
2020-03-17 20:23:46 -0700

    updated database

--- database/index.js
+++ database/index.js
@@ -3,7 +3,7 @@ let connection = mysql.createConnection({
   host: 'localhost',
   user: 'root',
   password: 'password',
-  database: 'item_information'
+  database: 'product_images'
 });
 
 connection.connect((err) => {
@@ -18,7 +18,7 @@ i will use this fn inside my /id/thumbnail endpoint and provide Damien with: ima
 //refactor fn name 'getImage'
 //refactor req to select url, styleId from table where styleId = (expected styleId?)
 let getSmallImage = function(callback) {
-  let req = `SELECT * FROM small_images`;
+  let req = `SELECT * FROM small_images where styleId = 30`;
   connection.query(req, (err, imgData) => {
     if (err) {
       callback(err);
