002093375619520860c414085cf1da88d5d8c6e4
Abraham Esparza <abrahamesparza.us@gmail.com>
2020-03-31 18:21:50 -0700

    udpate gitignore

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+let path = require('path');
+let src = path.join(__dirname, '/client/src');
+let dist = path.join(__dirname, '/client/dist');
+
+module.exports = {
+  entry: `${src}/index.jsx`,
+  output: {
+    filename: 'bundle.js',
+    path: dist
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx$/,
+        loader: 'babel-loader',
+        options: {
+          presets: ['@babel/preset-react']
+        }
+      }
+    ]
+  },
+  watch: true, //actively watches for changes
+  mode: 'production' //falls on production when not set, but setting eliminates warnings from webpack run
+};
