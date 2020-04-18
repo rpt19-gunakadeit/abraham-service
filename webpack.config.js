6517bb00123ff5a2c0bb07f2c34888f9340bbf79
Abraham Esparza <abrahamesparza.us@gmail.com>
2020-04-18 09:08:08 -0700

    style-loader webpack err

--- webpack.config.js
+++ webpack.config.js
@@ -19,13 +19,10 @@ module.exports = {
       },
       {
         test: /\.css$/,
-        loader: ['style-loader', 'css-loader'],
+        loader: ['style-loader','css-loader'],
         resolve: {
-          extensions: ['', '.js', '.jsx', '.css'],
-          modulesDirectories: [
-            'node_modules'
-          ]
-      }
+          extensions: ['.js', '.jsx', '.css']
+        }
       },
     ]
   },
